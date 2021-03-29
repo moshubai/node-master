ff370286231992266890dbf6bb87a6124180b209
moshubai <939761443@qq.com>
2021-03-29 17:50:54 +0800

    手写测试

--- testNow/__test__/index.spec.js
+++ testNow/__test__/index.spec.js
@@ -1,9 +1,35 @@
-
-test('测试文件名生成', () => {
+const fs = require('fs')
+test('集成测试', () => {
+    // 准备环境
+    // 删除测试文件夹
+    fs.rmdirSync(__dirname + '/data/__test__', {
+        recursive: true
+    })
     const src = new (require('../index'))()
-    const ret = src.getTestFileName('/abc/class.js')
-
-    console.log('getTestFileName',ret); //xu-log
-    expect(ret)
-    .toBe('/abc/__test__/class.spec.js')
+    src.getJestSource(__dirname + '/data')
 });
+
+// test('测试img', () => {
+//     const src = new (require('../index'))()
+//     const ret = src.getTestSource('fun','class')
+//     console.log('ret',ret); //xu-log
+//     expect(ret)
+//     .toBe(`
+// test('Test fun',()=>{
+// const fun = require('../class')
+// const res = fun()
+// //expect(ret)
+// // .toBe('Test')
+// })
+// `)
+// });
+
+
+// test('测试文件名生成', () => {
+//     const src = new (require('../index'))()
+//     const ret = src.getTestFileName('/abc/class.js')
+
+//     console.log('getTestFileName',ret); //xu-log
+//     expect(ret)
+//     .toBe('/abc/__test__/class.spec.js')
+// });
--- testNow/index.js
+++ testNow/index.js
@@ -1,6 +1,68 @@
 const path = require('path')
-
+const fs = require('fs')
 module.exports = class TestNow {
+
+
+
+
+
+    getJestSource(sourcePath = path.resolve('./')) {
+        const testPath = `${sourcePath}/__test__`
+        if (!fs.existsSync(testPath)) {
+            fs.mkdirSync(testPath)
+        }
+
+        let list = fs.readdirSync(sourcePath)
+        list
+            .map(v => `${sourcePath}/${v}`)
+            .filter(v => fs.statSync(v).isFile())
+            .filter(v => v.indexOf('.spec') === -1)
+            .map(v => this.getTestFile(v))
+    }
+
+    getTestFile(fileName) {
+        const testFileName = this.getTestFileName(fileName)
+        // 判断此文件是否存在
+        if (fs.existsSync(testFileName)) {
+            return
+        }
+        const me = require(fileName)
+        let source;
+        if (typeof me === 'object') {
+            source = Object.keys(me)
+                .map(v => this.getTestSource(v, path.basename(fileName), true))
+                .join('\n')
+        } else if (typeof me === 'function') {
+            const baseName = path.basename(fileName)
+            source = this.getTestSource(baseName.replace('js', ''), baseName)
+        }
+        fs.writeFileSync(testFileName, source)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    getTestSource(methodName, classFile, isClass = false) {
+        console.log('getTestSource:', methodName)
+        return `
+test('${'Test ' + methodName}',()=>{
+const ${isClass ? '{' + methodName + '}' : methodName} = require('${'../' + classFile}')
+const res = ${methodName}()
+//expect(ret)
+// .toBe('Test')
+})
+`
+    }
+
     /**
      * 
      * 
